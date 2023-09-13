5d23f88a9862cbe6dd5e1370554dfc339ef7f357
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-09-12 21:33:53 -0300

    Initial Commit

---
+++ SwiftFulCrypto/SwiftFulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFulCryptoApp.swift
+//  SwiftFulCrypto
+//
+//  Created by Christian Manzaraz on 12/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
